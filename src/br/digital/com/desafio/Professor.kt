f7ee55334e417a66a7d0da2d400c5a7e8f4e8ee9
Ananda Câmara <ananda.lr.camara@gmail.com>
2020-09-26 20:17:47 -0300

    Parte C - Classe Professor e equals()

---
+++ src/br/digital/com/desafio/Professor.kt
@@ -0,0 +1,11 @@
+package br.digital.com.desafio
+
+class Professor (var nome: String, var sobrenome: String, var tempoDeCasa: Int, val codigo: Int){
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Professor) return false
+        if (other.codigo == codigo) return true
+        return false
+    }
+}
