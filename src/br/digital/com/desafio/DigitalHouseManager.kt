b807f845261a3a249b75944b0da48153717a1b74
Ananda Câmara <ananda.lr.camara@gmail.com>
2020-09-26 20:47:59 -0300

    Parte G - Classe DigitalHouseManager

---
+++ src/br/digital/com/desafio/DigitalHouseManager.kt
@@ -0,0 +1,8 @@
+package br.digital.com.desafio
+
+class DigitalHouseManager {
+    lateinit var alunos: MutableMap<Int, Aluno>
+    lateinit var professores: Map<Int, Professor>
+    lateinit var cursos: Map<Int, Curso>
+    lateinit var matriculas: List<Matricula>
+}
